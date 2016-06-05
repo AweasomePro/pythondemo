a524ab303b403dc0bd729090d68816b6eb36ad5f
XiuWuZhuo <nimdanoob@163.com>
2016-06-05 18:21:36 +0800

    demo 配置

--- hotelDjango_project/hotelDjango_project/settings.py
+++ hotelDjango_project/hotelDjango_project/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '4n+04gs)1-&^9y#9q5)jg))nc9i%yalx$nx2i+r4$kg#l!2y#3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# 允许你设置哪些域名可以访问，即使在Apache中绑定了，这里不允许的话，也是不能访问的
+# 当 DEBUG = FALSE时，这个是必填项，如果不想输入，可以用*来表示允许所有
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,5 +124,30 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+# static 是静态文件所有目录，比如 jquery.js, bootstrap.min.css 等文件。
+# 一般来说我们只要把静态文件放在 APP 中的 static 目录下，
+# 部署时用 python manage.py collectstatic 就可以把静态文件收集到 STATIC_ROOT 目录，
+# 但是有时我们有一些共用的静态文件，这时候可以设置 STATICFILES_DIRS 另外弄一个文件夹，如下：
+#STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "common_static"),
+#     '/var/www/static/',
+#       )
 STATIC_URL = '/static/'
+# 当运行 python manage.py collectstatic 的时候
+# STATIC_ROOT 文件夹 是用来将所有STATICFILES_DIRS中所有文件夹中的文件，以及各app中static中的文件都复制过来
+# 把这些文件放到一起是为了用apache等部署的时候更方便
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# 其它 存放静态文件的文件夹，可以用来存放项目中公用的静态文件，里面不能包含 STATIC_ROOT
+# 如果不想用 STATICFILES_DIRS 可以不用，都放在 app 里的 static 中也可以
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "common_static"),
+    # '/path/to/others/static/',  # 用不到的时候可以不写这一行
+)
+
+# 这个是默认设置，Django 默认会在 STATICFILES_DIRS中的文件夹 和 各app下的static文件夹中找文件
+# 注意有先后顺序，找到了就不再继续找了
+# STATICFILES_FINDERS ＝ (
+#     "django.contrib.staticfiles.finders.FileSystemFinder",
+#     "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+# )
--- hotelDjango_project/test.py
+++
@@ -1,5 +0,0 @@
-from hotel.models import  Person
-from hotelDjango_project import settings
-settings.configure()
-Person.objects.create(name="zxw",age=24)
-Person.objects.get(name="zhuoxiuwu")
