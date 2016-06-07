2bdee5a28e79ad788753bf15366dfbf3ad7dc508
XiuWuZhuo <nimdanoob@163.com>
2016-06-07 17:14:44 +0800

    url add /

--- demo/hotelBooking/urls.py
+++ demo/hotelBooking/urls.py
@@ -3,7 +3,7 @@ from hotelBooking import views
 
 
 urlpatterns = [
-    url(r'^login$', views.member_login),
-    url(r'^register$', views.member_register),
+    url(r'^login/$', views.member_login),
+    url(r'^register/$', views.member_register),
     url(r'^ems/member/regist', views.send_regist_sms),
 ]
