1ec47c72cddac74a524db1aa29815f9b6d67e9e4
XiuWuZhuo <nimdanoob@163.com>
2016-06-07 17:52:52 +0800

    add modified

--- demo/hotelBooking/serializers.py
+++ demo/hotelBooking/serializers.py
@@ -11,18 +11,6 @@ from .models import  Member
 
 class MemberSerializer(serializers.HyperlinkedModelSerializer):
 
-    def create(self, validated_data):
-        return Member.objects.create(**validated_data)
-
-    def update(self,instance,validated_data):
-        # instance.title = validated_data.get('title', instance.title)
-        # instance.code = validated_data.get('code', instance.code)
-        # instance.linenos = validated_data.get('linenos', instance.linenos)
-        # instance.language = validated_data.get('language', instance.language)
-        # instance.style = validated_data.get('style', instance.style)
-        # instance.save()
-        return instance
-
     class Meta:
         model = Member
         fields = ('name','email','phoneNumber','registerTime')
