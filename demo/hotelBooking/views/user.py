6d6f4d796a0deff1a851eebb9cb6ced14d1914e9
nimdanoob <nimdanoob@163.com>
2016-07-25 19:57:36 +0800

    default sms code success

--- demo/hotelBooking/views/user.py
+++ demo/hotelBooking/views/user.py
@@ -58,7 +58,7 @@ class UserViewSet(UpdateModelMixin,viewsets.GenericViewSet):
         if User.existPhoneNumber(phone_number,raise_exception=False):
             return DefaultJsonResponse(code=appcodes.CODE_SMS_ERROR, message="手机号已存在")
         # UserCheck.validate_pwd(password)
-        if (sms.verify_sms_code(phone_number,sms_code)[0]):
+        if (sms.verify_sms_code(phone_number,sms_code)[0] or True):
             try:
                 # todo 测试
                 if request.POST.get('test',None):
@@ -108,7 +108,7 @@ class UserViewSet(UpdateModelMixin,viewsets.GenericViewSet):
         print('phone is {}'.format(phone_number))
         try:
             user = User.objects.get(phone_number=phone_number)
-            if (smsCode and user.check_smscode(phone_number,smsCode)):
+            if (smsCode and user.check_smscode(phone_number,smsCode) or True):
                 payload = jwt_payload_handler(user)
                 token = jwt_encode_handler(payload)
                 if(user.role == User.CUSTOMER):
